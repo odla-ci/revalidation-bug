181b5d0536330a5ffd25cf3cc60f6e2d4889cc6e
Mikael Gustafsson <mikael@weahead.se>
2021-02-03 09:29:31 +0100

    add unique id for each getStaticProp execution

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,18 +1,21 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { FC } from 'react'
+import {v4} from "uuid"
 
 import Layout from '../../components/Layout'
 import { Post } from '../../types'
 
 type Props = {
-  post: Post
+  post: Post,
+  gspId: string
 }
 
-const PostPage: FC<Props> = ({ post }) => {
+const PostPage: FC<Props> = ({ post, gspId }) => {
   return (
     <Layout
     title={post.title}
     >
+      <h2>gspId: {gspId}</h2>
       <dl>
         <dt>
           id
@@ -53,18 +56,23 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string }
+  const gspId = v4() // Unique id for this executuon of GSP
 
   try {
+    console.time(`${id} - ${gspId}`)
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
     const post = await res.json()
     
     return {
       revalidate: 10,
       props: {
-        post
+        post,
+        gspId
       }
     }
   } catch (error) {
     throw new Error(`Error fetching post ${id}: ${error}`)
+  } finally {
+    console.timeEnd(`${id} - ${gspId}`)
   }
 }
